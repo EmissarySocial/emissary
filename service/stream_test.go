874aa0e2d1c2625f04bc55ef81555723f62cb64a
Ben Pate <ben@pate.org>
2020-04-19 16:56:47 -0600

    Update tests

--- service/stream_test.go
+++ service/stream_test.go
@@ -4,15 +4,16 @@ import (
 	"testing"
 
 	"github.com/benpate/data/expression"
-	"github.com/benpate/data/mock"
+	"github.com/benpate/data/mockdb"
+	"github.com/benpate/data/option"
 	"github.com/benpate/ghost/model"
 	"github.com/stretchr/testify/assert"
 	"go.mongodb.org/mongo-driver/bson/primitive"
 )
 
-func TestStream1(t *testing.T) {
+func TestStream_ReadWrite(t *testing.T) {
 
-	datasource := mock.New()
+	datasource := mockdb.New()
 
 	factory := NewFactory(datasource)
 
@@ -20,15 +21,15 @@ func TestStream1(t *testing.T) {
 	stream1 := service.New()
 
 	stream1.StreamID = primitive.NewObjectID()
-	stream1.Label = "My First Stream"
-	stream1.Token = "my-first-stream"
+	stream1.Label = "My New Stream"
+	stream1.Token = "my-new-stream"
 
 	if err := service.Save(stream1, "This is the first record I'm going to save."); err != nil {
 		t.Error(err)
 		return
 	}
 
-	stream2, err := service.Load(expression.New("token", "=", "my-first-stream"))
+	stream2, err := service.Load(expression.New("token", "=", "my-new-stream"))
 
 	if err != nil {
 		t.Error(err)
@@ -36,42 +37,63 @@ func TestStream1(t *testing.T) {
 	}
 
 	assert.Equal(t, stream1.StreamID, stream2.StreamID)
-	assert.Equal(t, "My First Stream", stream2.Label)
-	assert.Equal(t, "my-first-stream", stream2.Token)
+	assert.Equal(t, "My New Stream", stream2.Label)
+	assert.Equal(t, "my-new-stream", stream2.Token)
 }
 
-func TestStream2(t *testing.T) {
+func TestStream_List(t *testing.T) {
 
 	service := getTestStreamService()
+	stream := service.New()
 
-	criteria := expression.And()
+	it, err := service.List(nil, option.SortDesc("token"))
 
-	service.List(criteria)
+	assert.Nil(t, err)
+
+	assert.True(t, it.Next(stream))
+	assert.Equal(t, "3-my-third-stream", stream.Token)
+
+	assert.True(t, it.Next(stream))
+	assert.Equal(t, "2-my-second-stream", stream.Token)
+
+	assert.True(t, it.Next(stream))
+	assert.Equal(t, "1-my-first-stream", stream.Token)
+
+	assert.False(t, it.Next(stream))
+	assert.False(t, it.Next(stream))
+	assert.False(t, it.Next(stream))
+	assert.False(t, it.Next(stream))
 }
 
 func getTestStreamService() Stream {
 
-	data := []model.Stream{
-		model.Stream{
-			Label: "My First Stream",
-			Token: "my-first-stream",
-		},
-		model.Stream{
-			Label: "My Second Stream",
-			Token: "my-second-stream",
-		},
-		model.Stream{
-			Label: "My Third Stream",
-			Token: "my-third-stream",
-		},
-	}
-
-	datasource := mock.New()
+	// Create service
+	datasource := mockdb.New()
 	factory := NewFactory(datasource)
 	service := factory.Stream()
 
+	// Initial data to load
+	data := []*model.Stream{
+		{
+			StreamID: primitive.NewObjectID(),
+			Label:    "My First Stream",
+			Token:    "1-my-first-stream",
+		},
+		{
+			StreamID: primitive.NewObjectID(),
+			Label:    "My Second Stream",
+			Token:    "2-my-second-stream",
+		},
+		{
+			StreamID: primitive.NewObjectID(),
+			Label:    "My Third Stream",
+			Token:    "3-my-third-stream",
+		},
+	}
+
+	// Populate datasource
 	for _, record := range data {
-		if err := service.Save(&record, "comment"); err != nil {
+		if err := service.Save(record, "comment"); err != nil {
 			panic(err)
 		}
 	}
