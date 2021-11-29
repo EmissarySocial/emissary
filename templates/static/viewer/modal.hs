a846977d78c8a6249481a35b464f6a830fe8c19e
Ben Pate <ben@pate.org>
2021-11-29 11:46:55 -0700

    Font Awesome 6.0
    - adding FondAwesome 6.0 (free version) for now.  Final icons to be determined later

--- templates/static/viewer/modal.hs
+++ templates/static/viewer/modal.hs
@@ -1,20 +1,25 @@
 behavior AsModal(url)
 
-    init
-        put "body" into [@hx-target]
-        put "beforeend" into [@hx-swap]
-        put "false" into [@hx-push-url]
-        put "true" into [@data-preload]
+	init
+		put "body" into [@hx-target]
+		put "beforeend" into [@hx-swap]
+		put "false" into [@hx-push-url]
+		put "true" into [@data-preload]
 
-        call htmx.process(me)
-    end
+		call htmx.process(me)
+	end
 end
 
-on closeModal(event)
-    if #modal is not empty then 
-        add .closing to #modal
-        set window.location to event.detail.nextPage unless no event.detail.nextPage
-        settle
-        remove #modal
-    end
+on closeModal(nextPage)
+
+	if #modal is not empty then 
+		add .closing to #modal
+		settle
+		remove #modal
+	end
+
+	if nextPage is not empty then 
+		set the window's location to nextPage
+	end
+
 end
