f69fb12495d587339ab6f9851a9ca7677b2c7f4e
Ben Pate <ben@pate.org>
2021-12-14 10:37:16 -0700

    Remove dead code

--- model/resourceKey.go
+++
@@ -1,8 +0,0 @@
-package model
-
-// ResourceKey represents one decryption key
-type ResourceKey struct {
-	ResourceKeyID string `json:"resourceId" bson:"_id"`
-	Label         string `json:"label"      bson:"label"`
-	EncryptedKey
-}
