f8e2cb56c524fcdc9adf12b0aa82087f179983ec
Ben Pate <ben@pate.org>
2022-01-18 08:51:26 -0700

    Remove dead code

--- role/inroom.go
+++
@@ -1,8 +0,0 @@
-package role
-
-import "github.com/labstack/echo/v4"
-
-// InRoom returns TRUE if the provided object is contained in a "stage" that is accessible by the current context
-func InRoom(context echo.Context) bool {
-	return true
-}
