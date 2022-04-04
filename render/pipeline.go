688915ab6fb5841b5dfb4464b89d19976f13b0ea
Ben Pate <ben@pate.org>
2022-04-04 11:55:32 -0600

    Update pipeline.go

--- render/pipeline.go
+++ render/pipeline.go
@@ -7,6 +7,26 @@ import (
 	"github.com/benpate/derp"
 )
 
+// NewPipeline converts an array of maps into an array of steps
+func NewPipeline(factory Factory, steps []datatype.Map) ([]Step, error) {
+
+	const location = "render.NewPipeline"
+
+	result := make([]Step, len(steps))
+
+	for index := range steps {
+		step, err := NewStep(factory, steps[index])
+
+		if err != nil {
+			return result, derp.Wrap(err, location, "Error parsing step", index, steps[index])
+		}
+
+		result[index] = step
+	}
+
+	return result, nil
+}
+
 // DoPipeline executes a series of RenderSteps on a particular Stream
 func DoPipeline(renderer Renderer, buffer io.Writer, steps []datatype.Map, method ActionMethod) error {
 
