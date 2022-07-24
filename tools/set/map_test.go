d577c1e3cd7428424eebc1e07e0d2aca6e121c1d
Ben Pate <ben@pate.org>
2022-07-24 17:46:28 -0600

    Better tests

--- tools/set/map_test.go
+++ tools/set/map_test.go
@@ -3,7 +3,7 @@ package set
 import (
 	"testing"
 
-	"github.com/davecgh/go-spew/spew"
+	"github.com/stretchr/testify/require"
 )
 
 func Test_Map(t *testing.T) {
@@ -13,9 +13,20 @@ func Test_Map(t *testing.T) {
 	s.Put(testPerson{id: "1", name: "Sarah", email: "sarah@sky.net"})
 	s.Put(testPerson{id: "2", name: "John", email: "john@sky.net"})
 
-	spew.Dump(s)
+	require.Equal(t, 2, s.Len())
 
-	v, err := s.Get("1")
+	one, err := s.Get("1")
+	require.NoError(t, err)
+	require.Equal(t, "Sarah", one.name)
+	require.Equal(t, "sarah@sky.net", one.email)
 
-	spew.Dump(v, err)
+	two, err := s.Get("2")
+	require.NoError(t, err)
+	require.Equal(t, "John", two.name)
+	require.Equal(t, "john@sky.net", two.email)
+
+	three, err := s.Get("3")
+	require.Error(t, err)
+	require.Equal(t, "", three.name)
+	require.Equal(t, "", three.email)
 }
