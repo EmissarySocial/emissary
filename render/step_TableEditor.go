f298cd0353f329d6fe28821ac41d0e92af2a474d
Ben Pate <ben@pate.org>
2022-09-05 08:57:40 -0600

    Use new Table widget

--- render/step_TableEditor.go
+++ render/step_TableEditor.go
@@ -2,14 +2,12 @@ package render
 
 import (
 	"io"
-	"reflect"
 
 	"github.com/benpate/derp"
 	"github.com/benpate/form"
-	"github.com/benpate/html"
 	"github.com/benpate/rosetta/convert"
 	"github.com/benpate/rosetta/maps"
-	"github.com/benpate/rosetta/schema"
+	"github.com/benpate/table"
 	"github.com/labstack/echo/v4"
 )
 
@@ -21,21 +19,25 @@ type StepTableEditor struct {
 func (step StepTableEditor) Get(renderer Renderer, buffer io.Writer) error {
 
 	const location = "render.StepTableEditor.Get"
+	var err error
 
 	s := renderer.schema()
 	targetURL := "/" + renderer.Token() + "/" + renderer.ActionID()
+	factory := renderer.factory()
 
-	editRow, ok := convert.IntOk(renderer.QueryParam("edit"), 0)
+	t := table.New(&s, &step.Form, renderer.object(), step.Path, factory.Icons(), targetURL)
+	t.UseLookupProvider(factory.LookupProvider())
+	t.AllowAll()
 
-	if !ok {
-		editRow = -1
+	if editRow, ok := convert.IntOk(renderer.QueryParam("edit"), 0); ok {
+		err = t.DrawEdit(editRow, buffer)
+	} else if add := renderer.QueryParam("add"); add != "" {
+		err = t.DrawAdd(buffer)
+	} else {
+		err = t.DrawView(buffer)
 	}
 
-	if err := step.drawTable(&s, renderer.object(), targetURL, editRow, buffer); err != nil {
-		return derp.Wrap(err, location, "Error building HTML")
-	}
-
-	return nil
+	return derp.Wrap(err, location, "Error drawing table", step.Path)
 }
 
 func (step StepTableEditor) UseGlobalWrapper() bool {
@@ -81,7 +83,7 @@ func (step StepTableEditor) Post(renderer Renderer) error {
 		// Try to delete an existing record
 	} else if delete := renderer.QueryParam("delete"); delete != "" {
 
-		table, _, err := s.Get(object, step.Path)
+		table, err := s.Get(object, step.Path)
 
 		if err != nil {
 			return derp.Wrap(err, location, "Error locating table in data object")
@@ -107,179 +109,14 @@ func (step StepTableEditor) Post(renderer Renderer) error {
 	// Once we're done, re-render the table and send it back to the client
 	targetURL := "/" + renderer.Token() + "/" + renderer.ActionID()
 
-	if err := step.drawTable(&s, renderer.object(), targetURL, -1, renderer.context().Response().Writer); err != nil {
+	factory := renderer.factory()
+	t := table.New(&s, &step.Form, renderer.object(), step.Path, factory.Icons(), targetURL)
+	t.UseLookupProvider(factory.LookupProvider())
+	t.AllowAll()
+
+	if err := t.DrawView(renderer.context().Response().Writer); err != nil {
 		return derp.Wrap(err, location, "Error building HTML")
 	}
-
-	return nil
-}
-
-func (step StepTableEditor) drawTable(s *schema.Schema, object any, targetURL string, editRow int, buffer io.Writer) error {
-
-	const location = "render.StepTableEditor.view"
-
-	// Try to locate (and validate) that we have a usable schema for a table
-	value, tableElement, err := s.Get(object, step.Path)
-
-	if err != nil {
-		return derp.Wrap(err, location, "Failed to locate table schema", step.Path)
-	}
-
-	arrayElement, ok := tableElement.(schema.Array)
-
-	if !ok {
-		return derp.NewInternalError(location, "Table schema must be an array", step.Path, tableElement)
-	}
-
-	arraySchema := schema.New(arrayElement)
-
-	// Begin rendering the table
-	b := html.New()
-
-	b.Div().Role("table").
-		Data("hx-target", "this").
-		Data("hx-swap", "outerHTML").
-		Data("hx-push_url", "false")
-
-	b.Div().Role("row")
-	for _, field := range step.Form.Children {
-		b.Div().Role("columnheader").InnerHTML(field.Label).Close()
-	}
-	b.Div().Role("columnheader").Close() // This will be the "actions" column
-
-	b.Close() // .row
-
-	length := convert.SliceLength(value)
-	valueOf := reflect.ValueOf(value)
-
-	for index := 0; index < length; index++ {
-
-		indexString := convert.String(index)
-		row, rowElement, err := arraySchema.Get(valueOf, indexString)
-
-		if err != nil {
-			return derp.Wrap(err, location, "Failed to locate row schema", step.Path, index)
-		}
-
-		rowSchema := schema.New(rowElement)
-
-		if index == editRow {
-
-			if err := step.drawEditRow(&rowSchema, row, targetURL, indexString, b.SubTree()); err != nil {
-				return derp.Wrap(err, location, "Failed to draw edit row", step.Path, index)
-			}
-
-		} else {
-
-			if err := step.drawViewRow(&rowSchema, row, targetURL, indexString, b.SubTree()); err != nil {
-				return derp.Wrap(err, location, "Failed to draw row", step.Path, index)
-			}
-		}
-	}
-
-	// If we're not editing an existing row, then let users add a new row
-	if editRow == -1 {
-		step.drawAddRow(s, targetURL, length, b.SubTree())
-	}
-
-	b.CloseAll()
-
-	buffer.Write(b.Bytes())
-	return nil
-}
-
-func (step StepTableEditor) drawAddRow(s *schema.Schema, targetURL string, index int, b *html.Builder) {
-
-	b.Form("", "").
-		Data("hx-post", targetURL+"?edit="+convert.String(index)).
-		Role("row").
-		Class("add")
-
-	// b.Div().Role("row").Class("add")
-
-	for _, field := range step.Form.Children {
-		b.Div().Role("cell")
-		field.WriteHTML(s, nil, nil, b.SubTree())
-		b.Close() // .cell
-	}
-
-	b.Div().Role("cell").Class("align-right")
-	b.Button().Class("primary", "text-sm").InnerHTML("Add").Close()
-
-	b.Close() // .cell
-	// b.Close() // .row
-	b.Close() // form
-}
-
-func (step StepTableEditor) drawEditRow(rowSchema *schema.Schema, row any, targetURL string, index string, b *html.Builder) error {
-
-	b.Form("", "").
-		Role("row").
-		Data("hx-post", targetURL+"?edit="+index)
-
-	// b.Div().Role("row").Class("edit")
-
-	for _, field := range step.Form.Children {
-		b.Div().Role("cell")
-		field.WriteHTML(rowSchema, nil, row, b.SubTree())
-		b.Close() // .cell
-	}
-
-	// Write actions column
-	b.Div().Role("cell").Class("align-right")
-	b.Button().
-		Type("submit").
-		Class("primary", "text-sm").
-		InnerHTML("Save").Close()
-
-	b.Button().
-		Type("button").
-		Class("btn", "text-sm").
-		Data("hx-get", targetURL).
-		InnerHTML("Cancel").Close()
-	b.Close() // .cell
-	// b.Close() // .row
-	b.Close() // form
-
-	return nil
-}
-
-func (step StepTableEditor) drawViewRow(rowSchema *schema.Schema, row any, targetURL string, index string, b *html.Builder) error {
-
-	b.Div().Role("row").
-		Class("hover-trigger").
-		Data("hx-get", targetURL+"?edit="+index).
-		Data("hx-trigger", "click")
-
-	for _, field := range step.Form.Children {
-		cellValue, _, err := rowSchema.Get(row, field.Path)
-
-		if err != nil {
-			return derp.Wrap(err, "render.StepTableEditor.drawEditRow", "Failed to locate cell schema", step.Path, index, field.Path)
-		}
-
-		b.Div().Role("cell").InnerHTML(convert.String(cellValue)).Close()
-	}
-
-	b.Div().Role("cell").Class("align-right")
-
-	b.Button().
-		Role("button").
-		Class("hover-reveal").
-		Data("hx-get", targetURL+"?edit="+index).
-		InnerHTML("Edit").
-		Close()
-
-	b.Button().
-		Role("button").
-		Class("text-red", "hover-reveal").
-		Data("hx-confirm", "Are you sure you want to delete this row?").
-		Data("hx-post", targetURL+"?delete="+index).
-		InnerHTML("Delete").
-		Close()
-
-	b.Close() // .cell
-	b.Close() // .row
 
 	return nil
 }
