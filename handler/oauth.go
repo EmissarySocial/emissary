5255f09512eb4259ae0be01031b389a33eea412c
Ben Pate <ben@pate.org>
2022-10-16 21:28:07 -0600

    OAuth routes

---
+++ handler/oauth.go
@@ -0,0 +1,44 @@
+package handler
+
+import (
+	"net/http"
+
+	"github.com/EmissarySocial/emissary/server"
+	"github.com/benpate/derp"
+	"github.com/labstack/echo/v4"
+)
+
+// GetOAuth looks up the OAuth provider and forwards to the appropriate endpoint
+func GetOAuth(fm *server.Factory) echo.HandlerFunc {
+
+	const location = "handler.GetOAuth"
+
+	return func(ctx echo.Context) error {
+
+		// Try to look up the factory for this domain
+		factory, err := fm.ByContext(ctx)
+
+		if err != nil {
+			return derp.Wrap(err, location, "Could not get factory", ctx.Request().URL.String())
+		}
+
+		// Get domain service and redirect URL
+		providerID := ctx.Param("provider")
+		domainService := factory.Domain()
+		redirectURL, err := domainService.OAuthCodeURL(providerID)
+
+		if err != nil {
+			return derp.Wrap(err, location, "Could not get redirect URL", providerID)
+		}
+
+		// Success!!
+		return ctx.Redirect(http.StatusTemporaryRedirect, redirectURL)
+	}
+}
+
+func OAuthRedirect(factory *server.Factory) echo.HandlerFunc {
+
+	return func(ctx echo.Context) error {
+		return nil
+	}
+}
---
+++ handler/setup_oauth.go
@@ -0,0 +1,137 @@
+package handler
+
+import (
+	"html/template"
+	"net/http"
+
+	"github.com/EmissarySocial/emissary/config"
+	"github.com/EmissarySocial/emissary/render"
+	"github.com/EmissarySocial/emissary/server"
+	"github.com/EmissarySocial/emissary/tools/dataset"
+	"github.com/EmissarySocial/emissary/tools/set"
+	"github.com/benpate/derp"
+	"github.com/benpate/form"
+	"github.com/benpate/rosetta/maps"
+	"github.com/labstack/echo/v4"
+)
+
+func SetupOAuthList(factory *server.Factory, templates *template.Template) echo.HandlerFunc {
+	return SetupPageGet(factory, templates, "oauth.html")
+}
+
+func SetupOAuthGet(factory *server.Factory, templates *template.Template) echo.HandlerFunc {
+
+	return func(ctx echo.Context) error {
+
+		// Locate the requested provider
+		oAuthProviders := set.Slice[form.LookupCode](dataset.Providers())
+		oAuthProviderID := ctx.Param("provider")
+		oAuthProvider, ok := oAuthProviders.Get(oAuthProviderID)
+
+		if !ok {
+			return derp.NewInternalError("setup.oauth.get", "Unable to find provider", oAuthProviderID)
+		}
+
+		// Build the custom form title
+		iconService := factory.Icons()
+		title := iconService.Get(oAuthProvider.Icon) + " " + oAuthProvider.Label + " - OAuth Setup"
+
+		// Load the requested connection from the domain configuration.
+		configuration := factory.Config()
+		connection, _ := configuration.Providers.Get(oAuthProviderID)
+		connection.ProviderID = oAuthProviderID
+
+		// Create the form to edit the data
+		editForm := setupOAuthForm(title)
+		formHTML, err := editForm.Editor(connection, nil)
+
+		if err != nil {
+			return derp.Wrap(err, "handler.SetupDomainGet", "Error generating form")
+		}
+
+		// Wrap the form in a modal dialog
+		result := render.WrapModalForm(ctx.Response(), "/oauth/"+oAuthProviderID, formHTML)
+
+		return ctx.HTML(200, result)
+	}
+}
+
+func SetupOAuthPost(factory *server.Factory, templates *template.Template) echo.HandlerFunc {
+
+	return func(ctx echo.Context) error {
+
+		// Validate provider ID
+		providers := set.Slice[form.LookupCode](dataset.Providers())
+		providerID := ctx.Param("provider")
+
+		if _, ok := providers.Get(providerID); !ok {
+			return derp.NewInternalError("setup.oauth.get", "Unable to find provider", providerID)
+		}
+
+		// Collect the updated connection information from the form post
+		data := maps.New()
+
+		if err := ctx.Bind(&data); err != nil {
+			return derp.Wrap(err, "handler.SetupOAuthPost", "Error binding form data")
+		}
+
+		// Load the requested connection from the domain configuration.
+		configuration := factory.Config()
+		connection, _ := configuration.Providers.Get(providerID)
+		connection.ProviderID = providerID
+
+		// Apply the POST data into the connection
+		editForm := setupOAuthForm("")
+		editForm.Do(data, &connection)
+
+		// Save/Delete the connection
+		if !connection.IsEmpty() {
+
+			// Save to configuration location
+			if err := factory.PutProvider(connection); err != nil {
+				return derp.Wrap(err, "handler.SetupOAuthPost", "Error saving connection", connection)
+			}
+
+		} else {
+
+			// Remove empty configurations
+			if err := factory.DeleteProvider(providerID); err != nil {
+				return derp.Wrap(err, "handler.SetupOAuthPost", "Error saving connection", providerID)
+			}
+		}
+
+		// Success!
+		render.CloseModal(ctx, "")
+		return ctx.NoContent(http.StatusOK)
+	}
+}
+
+func setupOAuthForm(title string) form.Form {
+
+	return form.Form{
+		Schema: config.ConnectionSchema(),
+		Element: form.Element{
+			Type:        "layout-vertical",
+			Label:       title,
+			Description: "These credentials should be obtained from the provider's website when you register this OAuth client with their API.",
+			Children: []form.Element{
+				{
+					Type:  "text",
+					Label: "Client ID",
+					Path:  "clientId",
+					Options: maps.Map{
+						"autocomplete": "off",
+					},
+				},
+				{
+					Type:  "text",
+					Label: "Client Secret",
+					Path:  "clientSecret",
+					Options: maps.Map{
+						"autocomplete": "off",
+					},
+				},
+			},
+		},
+	}
+}
