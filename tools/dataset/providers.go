b3a9bf51da7f180251fe6dc15afcf40f3c46eaf3
Ben Pate <ben@pate.org>
2022-10-16 21:21:50 -0600

    Add Providers dataset

---
+++ tools/dataset/providers.go
@@ -0,0 +1,46 @@
+package dataset
+
+import (
+	"github.com/benpate/form"
+)
+
+func Providers() []form.LookupCode {
+
+	return []form.LookupCode{
+		{
+			Value:       "FACEBOOK",
+			Label:       "Facebook",
+			Icon:        "facebook",
+			Description: "TBD",
+			Group:       "OAUTH",
+		},
+		{
+			Value:       "INSTAGRAM",
+			Label:       "Instagram",
+			Icon:        "instagram",
+			Description: "TBD",
+			Group:       "OAUTH",
+		},
+		{
+			Value:       "LINKEDIN",
+			Label:       "LinkedIn",
+			Icon:        "linkedin",
+			Description: "TBD",
+			Group:       "OAUTH",
+		},
+		{
+			Value:       "STRIPE",
+			Label:       "Stripe",
+			Icon:        "stripe",
+			Description: "To migrate from original API key",
+			Group:       "MANUAL",
+		},
+		{
+			Value:       "TWITTER",
+			Label:       "Twitter",
+			Icon:        "twitter",
+			Description: "Import your Twitter tweets and followers",
+			Group:       "MANUAL",
+		},
+	}
+}
