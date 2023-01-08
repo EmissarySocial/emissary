9cffc1f2cfd89f0610e9ab94717240ab64c0f847
Ben Pate <ben@pate.org>
2023-01-08 12:00:12 -0700

    User Accessors

--- model/user_accessors.go
+++ model/user_accessors.go
@@ -1,5 +1,11 @@
 package model
 
+import "go.mongodb.org/mongo-driver/bson/primitive"
+
+/*********************************
+ * Getter Methods
+ *********************************/
+
 func (user *User) GetBool(name string) bool {
 	switch name {
 	case "isOwner":
@@ -45,11 +51,82 @@ func (user *User) GetString(name string) string {
 	}
 }
 
-func (user *User) GetChild(name string) any {
+/*********************************
+ * Setter Methods
+ *********************************/
+
+func (user *User) SetBool(name string, value bool) bool {
+	switch name {
+	case "isOwner":
+		user.IsOwner = value
+		return true
+	}
+	return false
+}
+
+func (user *User) SetInt(name string, value int) bool {
+	switch name {
+	case "followerCount":
+		user.FollowerCount = value
+		return true
+	case "followingCount":
+		user.FollowingCount = value
+		return true
+	case "blockCount":
+		user.BlockCount = value
+		return true
+	}
+
+	return false
+}
+
+func (user *User) SetString(name string, value string) bool {
+	switch name {
+	case "userId":
+		if objectID, err := primitive.ObjectIDFromHex(value); err == nil {
+			user.UserID = objectID
+			return true
+		}
+	case "imageId":
+		if objectID, err := primitive.ObjectIDFromHex(value); err == nil {
+			user.ImageID = objectID
+			return true
+		}
+
+	case "displayName":
+		user.DisplayName = value
+		return true
+	case "statusMessage":
+		user.StatusMessage = value
+		return true
+	case "location":
+		user.Location = value
+		return true
+	case "emailAddress":
+		user.EmailAddress = value
+		return true
+	case "username":
+		user.Username = value
+		return true
+	case "profileUrl":
+		user.ProfileURL = value
+		return true
+	}
+
+	return false
+}
+
+/*********************************
+ * Tree Traversal
+ *********************************/
+
+func (user *User) GetChild(name string) (any, bool) {
 	switch name {
 	case "passwordReset":
-		return &user.PasswordReset
+		return &user.PasswordReset, true
+	case "links":
+		return &user.Links, true
 	default:
-		return nil
+		return nil, false
 	}
 }
