d325e56d48ba66dab970853165c4f841dfbe0b47
Ben Pate <ben@pate.org>
2023-01-13 11:48:37 -0700

    Caching improvements

--- render/step_ViewHTML.go
+++ render/step_ViewHTML.go
@@ -2,6 +2,8 @@ package render
 
 import (
 	"io"
+	"net/http"
+	"time"
 
 	"github.com/benpate/derp"
 )
@@ -14,10 +16,34 @@ type StepViewHTML struct {
 // Get renders the Stream HTML to the context
 func (step StepViewHTML) Get(renderer Renderer, buffer io.Writer) error {
 
-	header := renderer.context().Response().Header()
+	context := renderer.context()
+
+	requestHeader := context.Request().Header
+
+	// Validate If-None-Match Header
+	if etag := requestHeader.Get("If-None-Match"); etag != "" {
+		if etag == renderer.object().ETag() {
+			context.Response().WriteHeader(http.StatusNotModified)
+			return nil
+		}
+	}
+
+	// Validate If-Modified-Since Header
+	if modifiedSince := requestHeader.Get("If-Modified-Since"); modifiedSince != "" {
+		if modifiedSinceDate, err := time.Parse(time.RFC3339, modifiedSince); err == nil {
+			if modifiedSinceDate.UnixMilli() >= renderer.object().Updated() {
+				context.Response().WriteHeader(http.StatusNotModified)
+				return nil
+			}
+		}
+	}
+
+	header := context.Response().Header()
 
 	header.Set("Vary", "Cookie, HX-Request")
 	header.Set("Cache-Control", "private")
+
+	// TODO: LOW: We can do a better job with caching.  If a page is public, then caching should be public, too.
 
 	var filename string
 
@@ -28,9 +54,9 @@ func (step StepViewHTML) Get(renderer Renderer, buffer io.Writer) error {
 	}
 
 	// TODO: MEDIUM: Re-implement caching.  Will need to automatically compute the "Vary" header.
-	// object := renderer.object()
-	// header.Set("Last-Modified", time.UnixMilli(object.Updated()).Format(time.RFC3339))
-	// header.Set("ETag", object.ETag())
+	object := renderer.object()
+	header.Set("Last-Modified", time.UnixMilli(object.Updated()).Format(time.RFC3339))
+	header.Set("ETag", object.ETag())
 
 	if err := renderer.executeTemplate(buffer, filename, renderer); err != nil {
 		return derp.Wrap(err, "render.StepViewHTML.Get", "Error executing template")
