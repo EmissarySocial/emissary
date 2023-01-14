d05ecd1a5ceda50ada0aa66df7a601baa962e107
Ben Pate <ben@pate.org>
2023-01-13 19:03:24 -0700

    Remove List datastructure
    - in favor of sliceof.Type[T]

--- render/functions.go
+++ render/functions.go
@@ -53,51 +53,6 @@ func FuncMap(icons icon.Provider) template.FuncMap {
 			return template.CSS(value)
 		},
 
-		"head": func(slice List) Renderer { // Returns the first item in a resultSet
-			return slice[0]
-		},
-
-		"last": func(slice List) Renderer { // Returns the last item in a resultSet
-			return slice[len(slice)-1]
-		},
-
-		"tail": func(slice List) List { // Returns all but the first item in a resultSet
-			length := len(slice)
-			if length == 0 {
-				return List{}
-			}
-			return slice[1:]
-		},
-
-		"removeLast": func(slice List) List { // Returns all but the last item in a resultSet
-			length := len(slice)
-			if length == 0 {
-				return List{}
-			}
-			return slice[:length-1]
-		},
-
-		"reverse": func(slice List) List { // Returns a new resultSet with reverse ordering
-			length := len(slice)
-			result := make(List, length)
-			for index := range slice {
-				result[length-1-index] = slice[index]
-			}
-			return result
-		},
-
-		"isEmpty": func(slice List) bool { // Returns true if there are NO records in the resultset
-			return len(slice) == 0
-		},
-
-		"isSingle": func(slice List) bool { // Returns true if there are NO records in the resultset
-			return len(slice) == 1
-		},
-
-		"notEmpty": func(slice List) bool { // Returns true if there are records in the resultset
-			return len(slice) > 0
-		},
-
 		"json": func(value any) string {
 			result, _ := json.MarshalIndent(value, "", "  ")
 			return string(result)
