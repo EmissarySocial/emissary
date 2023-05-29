9e6b5c59320b63a4bdeb015c420209a6130747f5
Ben Pate <ben@pate.org>
2023-05-28 21:52:11 -0600

    Update messages to use string-based InReplyTo

--- model/message.go
+++ model/message.go
@@ -10,23 +10,23 @@ import (
 // Message represents a single item in a User's inbox or outbox.  It is loosely modelled on the MessageStreams
 // standard, and can be converted into a strict go-fed streams.Type object.
 type Message struct {
-	MessageID    primitive.ObjectID           `json:"messageId"    bson:"_id"`                    // Unique ID of the Message
-	UserID       primitive.ObjectID           `json:"userId"       bson:"userId"`                 // Unique ID of the User who owns this Message (in their inbox or outbox)
-	FolderID     primitive.ObjectID           `json:"folderId"     bson:"folderId,omitempty"`     // Unique ID of the Folder where this Message is stored
-	SocialRole   string                       `json:"socialRole"   bson:"socialRole,omitempty"`   // Role this message plays in social integrations ("Article", "Note", etc)
-	Origin       OriginLink                   `json:"origin"       bson:"origin,omitempty"`       // Link to the origin of this Message
-	URL          string                       `json:"url"          bson:"url,omitempty"`          // URL of this Message
-	Label        string                       `json:"label"        bson:"label,omitempty"`        // Label of this Message
-	Summary      string                       `json:"summary"      bson:"summary,omitempty"`      // Summary of this Message
-	ImageURL     string                       `json:"imageUrl"     bson:"imageUrl,omitempty"`     // URL of the image associated with this Message
-	AttributedTo sliceof.Object[PersonLink]   `json:"attributedTo" bson:"attributedTo,omitempty"` // List of people who are attributed to this Message
-	InReplyTo    sliceof.Object[DocumentLink] `json:"inReplyTo"    bson:"inReplyTo,omitempty"`    // List of messages that this message is in reply to
-	ContentHTML  string                       `json:"contentHtml"  bson:"contentHtml,omitempty"`  // HTML Content of the Message
-	ContentJSON  string                       `json:"contentJson"  bson:"contentJson,omitempty"`  // Original JSON message, used for reprocessing later.
-	Responses    ResponseSummary              `json:"responses"    bson:"responses,omitempty"`    // Summary counter of Responses to this Message
-	MyResponse   string                       `json:"myResponse"   bson:"myResponse,omitempty"`   // If the owner of this message has responded, then this field contains the responseType (Like, Dislike, Repost)
-	PublishDate  int64                        `json:"publishDate"  bson:"publishDate,omitempty"`  // Unix timestamp of the date/time when this Message was published
-	Rank         int64                        `json:"rank"         bson:"rank"`                   // Sort rank for this message (publishDate * 1000 + sequence number)
+	MessageID    primitive.ObjectID         `json:"messageId"    bson:"_id"`                    // Unique ID of the Message
+	UserID       primitive.ObjectID         `json:"userId"       bson:"userId"`                 // Unique ID of the User who owns this Message (in their inbox or outbox)
+	FolderID     primitive.ObjectID         `json:"folderId"     bson:"folderId,omitempty"`     // Unique ID of the Folder where this Message is stored
+	SocialRole   string                     `json:"socialRole"   bson:"socialRole,omitempty"`   // Role this message plays in social integrations ("Article", "Note", etc)
+	Origin       OriginLink                 `json:"origin"       bson:"origin,omitempty"`       // Link to the origin of this Message
+	URL          string                     `json:"url"          bson:"url,omitempty"`          // URL of this Message
+	Label        string                     `json:"label"        bson:"label,omitempty"`        // Label of this Message
+	Summary      string                     `json:"summary"      bson:"summary,omitempty"`      // Summary of this Message
+	ImageURL     string                     `json:"imageUrl"     bson:"imageUrl,omitempty"`     // URL of the image associated with this Message
+	AttributedTo sliceof.Object[PersonLink] `json:"attributedTo" bson:"attributedTo,omitempty"` // List of people who are attributed to this Message
+	InReplyTo    string                     `json:"inReplyTo"    bson:"inReplyTo,omitempty"`    // URL this message is in reply to
+	ContentHTML  string                     `json:"contentHtml"  bson:"contentHtml,omitempty"`  // HTML Content of the Message
+	ContentJSON  string                     `json:"contentJson"  bson:"contentJson,omitempty"`  // Original JSON message, used for reprocessing later.
+	Responses    ResponseSummary            `json:"responses"    bson:"responses,omitempty"`    // Summary counter of Responses to this Message
+	MyResponse   string                     `json:"myResponse"   bson:"myResponse,omitempty"`   // If the owner of this message has responded, then this field contains the responseType (Like, Dislike, Repost)
+	PublishDate  int64                      `json:"publishDate"  bson:"publishDate,omitempty"`  // Unix timestamp of the date/time when this Message was published
+	Rank         int64                      `json:"rank"         bson:"rank"`                   // Sort rank for this message (publishDate * 1000 + sequence number)
 
 	journal.Journal `json:"-" bson:"journal"`
 }
