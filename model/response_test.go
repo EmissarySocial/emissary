fc5902b938a3444d0d3068bcf6f1423b873305a4
Ben Pate <ben@pate.org>
2023-06-14 15:57:35 -0600

    Update test case

--- model/response_test.go
+++ model/response_test.go
@@ -14,21 +14,10 @@ func TestResponse(t *testing.T) {
 	tests := []tableTestItem{
 		{"responseId", "000000000000000000000001", nil},
 		{"type", ResponseTypeMention, nil},
-		{"value", "😀", nil},
-
-		{"actor.userId", "000000000000000000000002", nil},
-		{"actor.name", "ACTOR_ NAME", nil},
-		{"actor.profileUrl", "ACTOR_URL", nil},
-
-		{"message.id", "000000000000000000000005", nil},
-		{"message.url", "https://example/object", nil},
-		{"message.label", "DOC-LABEL", nil},
-		{"message.summary", "DOC-SUMMARY", nil},
-		{"message.imageUrl", "DOC-IMAGEURL", nil},
-		{"message.attributedTo.0.userId", "000000000000000000000004", nil},
-		{"message.attributedTo.0.name", "DOC-AUTHOR-NAME", nil},
-		{"message.attributedTo.0.profileUrl", "https://example/author", nil},
-		{"message.label", "OBJECT_NAME", nil},
+		{"actorId", "http://actor.com", nil},
+		{"objectId", "https://example/object", nil},
+		{"summary", "THIS_IS_A_SUMMARY", nil},
+		{"content", "😀", nil},
 	}
 
 	tableTest_Schema(t, &s, &response, tests)
