aa868e7de6eee08bff39b12d37deb8f9cf198991
Ben Pate <ben@pate.org>
2024-02-12 12:52:49 -0700

    Migrate *.json config files to *.hjson

--- model/bundle.go
+++ model/bundle.go
@@ -3,8 +3,8 @@ package model
 // Bundle represents a collection of files that are packaged into a Theme, Template, or Widget and are served as a single unit.
 // Recognized content types are automatically minified via github.com/tdewolff/minify/v2.
 type Bundle struct {
-	ContentType  string `json:"contentType"`
-	CacheControl string `json:"cacheControl"`
+	ContentType  string `json:"content-type"`
+	CacheControl string `json:"cache-control"`
 	Content      []byte `json:"-"`
 }
 
@@ -13,7 +13,7 @@ type Bundle struct {
 func (bundle Bundle) GetCacheControl() string {
 
 	if bundle.CacheControl == "" {
-		return "public, max-age=3600"
+		return "public, max-age=3600" // Default cache-control header. Cache resources for 1 hour.
 	}
 
 	return bundle.CacheControl
