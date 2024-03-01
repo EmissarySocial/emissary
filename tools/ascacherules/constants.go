99967e567d1e5d1412ed914529e1aa10de61cf46
Ben Pate <ben@pate.org>
2024-02-29 18:48:34 -0700

    Linting

--- tools/ascacherules/constants.go
+++ tools/ascacherules/constants.go
@@ -2,28 +2,28 @@ package ascacherules
 
 // second represents the number of seconds in a second
 // nolint:unused
-const second = 1 // nolint:unused - may not be used, but here anyway for completion
+const second = 1
 
 // minute represents the number of seconds in a minute
 // nolint:unused
-const minute = 60 // nolint:unused - may not be used, but here anyway for completion
+const minute = 60
 
 // hour represents the number of seconds in an hour
 // nolint:unused
-const hour = 60 * 60 // nolint:unused - may not be used, but here anyway for completion
+const hour = 60 * 60
 
 // day represents the number of seconds in a day
 // nolint:unused
-const day = 60 * 60 * 24 // nolint:unused - may not be used, but here anyway for completion
+const day = 60 * 60 * 24
 
 // week represents the number of seconds in a week
 // nolint:unused
-const week = 60 * 60 * 24 * 7 // nolint:unused - may not be used, but here anyway for completion
+const week = 60 * 60 * 24 * 7
 
 // month represents the number of seconds in 30 days.  No, not exactly a month, but close enough for ascache
 // nolint:unused
-const month = 60 * 60 * 24 * 30 // nolint:unused - may not be used, but here anyway for completion
+const month = 60 * 60 * 24 * 30
 
 // year represents the number of seconds in 365 days.  No, not exactly a year, but close enough for ascache
 // nolint:unused
-const year = 60 * 60 * 24 * 356 // nolint:unused - may not be used, but here anyway for completion
+const year = 60 * 60 * 24 * 356
