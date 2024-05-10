aae332d485c5b63b1ab09e1c945b829dbb6934f2
Ben Pate <ben@pate.org>
2024-05-10 07:29:42 -0600

    Refactor "Client" => "Connection"

---
+++ model/connection_constants.go
@@ -0,0 +1,9 @@
+package model
+
+// ConnectionProviderGiphy represents an API connection to the giphy.com service
+// for generating animated GIFs.
+const ConnectionProviderGiphy = "GIPHY"
+
+// ConnectionProviderStripe represents an API connection to the stripe.com service
+// for processing payments.
+const ConnectionProviderStripe = "STRIPE"
---
+++ model/connection_test.go
@@ -0,0 +1,23 @@
+package model
+
+import (
+	"testing"
+
+	"github.com/benpate/rosetta/schema"
+)
+
+func TestConnection(t *testing.T) {
+
+	origin := NewConnection()
+
+	s := schema.New(ConnectionSchema())
+
+	table := []tableTestItem{
+		{"connectionId", "123456781234567812345678", nil},
+		{"providerId", "GIPHY", nil},
+		{"data.random", "Any ValueGIPHY", nil},
+		{"active", "true", true},
+	}
+
+	tableTest_Schema(t, &s, &origin, table)
+}
