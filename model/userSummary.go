ac47c869d502939261297b03ae2a3953e1365e50
Ben Pate <ben@pate.org>
2024-09-08 21:14:47 -0600

    Add StatusMessage to UserSummary

--- model/userSummary.go
+++ model/userSummary.go
@@ -1,30 +1,34 @@
 package model
 
-import "go.mongodb.org/mongo-driver/bson/primitive"
+import (
+	"go.mongodb.org/mongo-driver/bson/primitive"
+)
 
 // UserSummary is used as a lightweight, read-only summary of a user record.
 type UserSummary struct {
-	UserID       primitive.ObjectID `bson:"_id"`
-	IconID       primitive.ObjectID `bson:"iconId"`
-	DisplayName  string             `bson:"displayName"`
-	EmailAddress string             `bson:"emailAddress"`
-	Username     string             `bson:"username"`
-	ProfileURL   string             `bson:"profileUrl"`
+	UserID        primitive.ObjectID `bson:"_id"`
+	IconID        primitive.ObjectID `bson:"iconId"`
+	DisplayName   string             `bson:"displayName"`
+	EmailAddress  string             `bson:"emailAddress"`
+	Username      string             `bson:"username"`
+	ProfileURL    string             `bson:"profileUrl"`
+	StatusMessage string             `bson:"statusMessage"`
 }
 
 func NewUserSummary() UserSummary {
 	return UserSummary{
-		UserID:       primitive.NewObjectID(),
-		IconID:       primitive.NilObjectID,
-		DisplayName:  "",
-		EmailAddress: "",
-		Username:     "",
-		ProfileURL:   "",
+		UserID:        primitive.NewObjectID(),
+		IconID:        primitive.NilObjectID,
+		DisplayName:   "",
+		EmailAddress:  "",
+		Username:      "",
+		ProfileURL:    "",
+		StatusMessage: "",
 	}
 }
 
 func UserSummaryFields() []string {
-	return []string{"_id", "displayName", "emailAddress", "username", "iconId", "profileUrl"}
+	return []string{"_id", "displayName", "emailAddress", "username", "iconId", "profileUrl", "statusMessage"}
 }
 
 func (userSummary UserSummary) Fields() []string {
