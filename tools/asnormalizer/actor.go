b6554139d23d574654f9ed6c5fc2778341323dd7
Ben Pate <ben@pate.org>
2024-09-28 11:20:53 -0600

    Added debugging into asnormalizer.Actor

--- tools/asnormalizer/actor.go
+++ tools/asnormalizer/actor.go
@@ -3,10 +3,18 @@ package asnormalizer
 import (
 	"github.com/benpate/hannibal/streams"
 	"github.com/benpate/hannibal/vocab"
+	"github.com/benpate/rosetta/convert"
+	"github.com/davecgh/go-spew/spew"
 )
 
 // Actor normalizes an Actor document
 func Actor(document streams.Document) map[string]any {
+
+	spew.Dump("NORMALIZE ACTOR ---", document.Value())
+
+	if result, ok := convert.MapOfAnyOk(document.Value); ok {
+		return result
+	}
 
 	result := map[string]any{
 
