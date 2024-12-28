79a164406765cf4f202ce3282143b2e2581be466
Ben Pate <ben@pate.org>
2024-12-28 16:23:38 -0700

    Remove debug code

--- build/step_Sleep.go
+++ build/step_Sleep.go
@@ -3,8 +3,6 @@ package build
 import (
 	"io"
 	"time"
-
-	"github.com/davecgh/go-spew/spew"
 )
 
 // StepSleep is a Step that sleeps for a determined period of time.
@@ -14,13 +12,11 @@ type StepSleep struct {
 }
 
 func (step StepSleep) Get(builder Builder, buffer io.Writer) PipelineBehavior {
-	spew.Dump("Sleeping", step.Duration)
 	time.Sleep(time.Duration(step.Duration) * time.Millisecond)
 	return nil
 }
 
 func (step StepSleep) Post(builder Builder, buffer io.Writer) PipelineBehavior {
-	spew.Dump("Sleeping", step.Duration)
 	time.Sleep(time.Duration(step.Duration) * time.Millisecond)
 	return nil
 }
