7f057465c47743cbc1480adbfd66b9728a7d29e1
Ben Pate <ben@pate.org>
2025-01-08 18:06:05 -0700

    Update test cases

--- model/searchTag_test.go
+++ model/searchTag_test.go
@@ -13,9 +13,8 @@ func TestSearchTag(t *testing.T) {
 
 	tests := []tableTestItem{
 		{"searchTagId", "000000000000000000000001", nil},
-		{"type", "GENRE", nil},
+		{"group", "GENRE", nil},
 		{"name", "MYTAG", nil},
-		{"description", "DESCRIPTION", nil},
 		{"colors.01", "#663399", nil},
 		{"colors.02", "#AABBCC", nil},
 		{"stateId", SearchTagStateAllowed, nil},
