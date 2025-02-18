7bafaf857a12d8f3999b5a7d34ece0027beef4aa
Ben Pate <ben@pate.org>
2025-02-17 19:39:24 -0700

    Remove debug code

--- _embed/templates/theme-global/embed/embed.js
+++ _embed/templates/theme-global/embed/embed.js
@@ -31,17 +31,6 @@
 		target.style.width = "100%";
 		target.style.height = event.data.height + "px";
 		target.style.overflow = "hidden";
-
-		// Send a response to the iframe that includes the color scheme of this page.
-		var computedStyle = getComputedStyle(target.parentElement)
-		console.log(computedStyle)
-
-		var response = {
-			backgroundColor: computedStyle.backgroundColor,
-			color: computedStyle.color,
-		}
-
-		target.contentWindow.postMessage(response, "*");
 	}
 
 	window.addEventListener("message", receiveMessage)
