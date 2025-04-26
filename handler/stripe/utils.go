ad4e463149ab0892dff59a73d188c3f078478602
Ben Pate <ben@pate.org>
2025-04-26 17:37:33 -0600

    Comments

--- handler/stripe/utils.go
+++ handler/stripe/utils.go
@@ -1,5 +1,6 @@
 package stripe
 
+//nolint:unused - It's okay if this is unused from time to time.
 func iif[T any](condition bool, trueValue T, falseValue T) T {
 	if condition {
 		return trueValue
