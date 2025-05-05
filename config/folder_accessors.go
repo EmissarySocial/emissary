14c617a98fe54bc0614e9b4280f5e0169c7c317e
Ben Pate <ben@pate.org>
2025-05-05 09:26:11 -0600

    Improve config validation
    - fix "adapter" validation errors

--- config/folder_accessors.go
+++ config/folder_accessors.go
@@ -2,32 +2,32 @@ package config
 
 import "github.com/benpate/rosetta/schema"
 
-func ReadableFolderSchema() schema.Element {
+func ReadableFolderSchema(location string) schema.Element {
 	return schema.Object{
 		Properties: schema.ElementMap{
 			"adapter":   schema.String{Required: true, Default: "EMBED", Enum: []string{"EMBED", "FILE", "GIT", "S3"}},
 			"location":  schema.String{Required: true},
-			"accessKey": schema.String{RequiredIf: "adapter is S3"},
-			"secretKey": schema.String{RequiredIf: "adapter is S3"},
-			"region":    schema.String{RequiredIf: "adapter is S3"},
-			"token":     schema.String{RequiredIf: "adapter is S3"},
-			"bucket":    schema.String{RequiredIf: "adapter is S3"},
-			"path":      schema.String{RequiredIf: "adapter is S3"},
+			"accessKey": schema.String{RequiredIf: location + ".adapter is S3"},
+			"secretKey": schema.String{RequiredIf: location + ".adapter is S3"},
+			"region":    schema.String{RequiredIf: location + ".adapter is S3"},
+			"token":     schema.String{RequiredIf: location + ".adapter is S3"},
+			"bucket":    schema.String{RequiredIf: location + ".adapter is S3"},
+			"path":      schema.String{RequiredIf: location + ".adapter is S3"},
 		},
 	}
 }
 
-func WritableFolderSchema() schema.Element {
+func WritableFolderSchema(location string) schema.Element {
 	return schema.Object{
 		Properties: schema.ElementMap{
 			"adapter":   schema.String{Required: true, Default: "FILE", Enum: []string{"FILE", "S3"}},
 			"location":  schema.String{Required: true},
-			"accessKey": schema.String{RequiredIf: "adapter is S3"},
-			"secretKey": schema.String{RequiredIf: "adapter is S3"},
-			"region":    schema.String{RequiredIf: "adapter is S3"},
-			"token":     schema.String{RequiredIf: "adapter is S3"},
-			"bucket":    schema.String{RequiredIf: "adapter is S3"},
-			"path":      schema.String{RequiredIf: "adapter is S3"},
+			"accessKey": schema.String{RequiredIf: location + ".adapter is S3"},
+			"secretKey": schema.String{RequiredIf: location + ".adapter is S3"},
+			"region":    schema.String{RequiredIf: location + ".adapter is S3"},
+			"token":     schema.String{RequiredIf: location + ".adapter is S3"},
+			"bucket":    schema.String{RequiredIf: location + ".adapter is S3"},
+			"path":      schema.String{RequiredIf: location + ".adapter is S3"},
 		},
 		Wildcard: schema.String{MaxLength: 1000},
 	}
