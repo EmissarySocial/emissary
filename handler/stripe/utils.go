32519bd188bfaffbd1106c4f048519db083a15ef
Ben Pate <ben@pate.org>
2025-05-08 19:45:24 -0600

    Remove dead code

--- handler/stripe/utils.go
+++
@@ -1,9 +0,0 @@
-package stripe
-
-// nolint:unused - It's okay if this is unused from time to time.
-func iif[T any](condition bool, trueValue T, falseValue T) T {
-	if condition {
-		return trueValue
-	}
-	return falseValue
-}
