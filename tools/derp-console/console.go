44d3739d59990b3d081c5e5bb1fe7cce70f1c7b0
Ben Pate <ben@pate.org>
2025-09-20 12:05:19 -0600

    linting

--- tools/derp-console/console.go
+++ tools/derp-console/console.go
@@ -19,8 +19,7 @@ func New() Console {
 }
 
 func (console Console) Report(err error) {
-
-	fmt.Println("")
+	_, _ = fmt.Println("")
 	console.report(err)
 }
 
@@ -32,36 +31,36 @@ func (console Console) report(err error) {
 	wrappedError := errors.Unwrap(err)
 
 	if wrappedError == nil {
-		red.Println("ROOT ERROR: ", derp.Message(err))
+		_, _ = red.Println("ROOT ERROR: ", derp.Message(err))
 
 	} else {
 		console.report(wrappedError)
-		blue.Println("- WRAPPED BY:", derp.Message(err))
+		_, _ = blue.Println("- WRAPPED BY:", derp.Message(err))
 	}
 
 	if code := derp.ErrorCode(err); code != 0 {
-		fmt.Print("- CODE:      ")
-		fmt.Println(code, "-", strings.TrimSpace(http.StatusText(code)))
+		_, _ = fmt.Print("- CODE:      ")
+		_, _ = fmt.Println(code, "-", strings.TrimSpace(http.StatusText(code)))
 	}
 
 	if location := derp.Location(err); location != "" {
-		fmt.Print("- LOCATION:  ")
-		fmt.Println(location)
+		_, _ = fmt.Print("- LOCATION:  ")
+		_, _ = fmt.Println(location)
 	}
 
 	if details := derp.Details(err); len(details) > 0 {
 		for _, detail := range details {
 
-			fmt.Print("- DETAIL:    ")
+			_, _ = fmt.Print("- DETAIL:    ")
 
 			switch typed := detail.(type) {
 
 			case string:
-				fmt.Println(typed)
+				_, _ = fmt.Println(typed)
 
 			default:
 				formatted, _ := json.Marshal(detail)
-				fmt.Println(string(formatted))
+				_, _ = fmt.Println(string(formatted))
 			}
 		}
 	}
