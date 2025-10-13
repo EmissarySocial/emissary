de6782048eb5aacb38f1382bdc9134d104a502ce
Ben Pate <ben@pate.org>
2025-10-13 10:19:52 -0600

    Remove dead code

--- service/icons.go
+++ service/icons.go
@@ -30,11 +30,6 @@ func (service Icons) Get(name string) string {
 		return service.get("arrow-up-right-square")
 	case "announce-fill":
 		return service.get("arrow-up-right-square-fill")
-		/*
-			case "announce":
-				return service.get("rocket-takeoff")
-			case "announce-fill":
-				return service.get("rocket-takeoff-fill")*/
 	case "archive":
 		return service.get("archive")
 	case "archive-fill":
