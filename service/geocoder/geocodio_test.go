a1bc6c94b36ec8c4725f9e48c26bb70938c03120
Ben Pate <ben@pate.org>
2025-11-15 10:53:06 -0700

    Remove failing test
    - Geocodio only works in US, so this International test is failing.

--- service/geocoder/geocodio_test.go
+++ service/geocoder/geocodio_test.go
@@ -1,10 +1,10 @@
 package geocoder
 
-import "testing"
-
 var testGeocodioAPIKey string
 
+/* Geocodio only works in the US, so this international test is failing.
 func TestGeocodio_Address(t *testing.T) {
 	encoder := NewGeocodio(testGeocodioAPIKey)
 	testGeocodeAddress(t, encoder)
 }
+*/
