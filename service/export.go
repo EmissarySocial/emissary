8f78168ad0b7577afa3afbeeb302548ad4e41a8d
Ben Pate <ben@pate.org>
2025-11-19 08:54:19 -0700

    Service placeholders

---
+++ service/export.go
@@ -0,0 +1,15 @@
+package service
+
+// Export service helps exports user data to another server
+type Export struct {
+}
+
+// NewExport returns a fully populated Export service
+func NewExport() Export {
+	return Export{}
+}
+
+/*
+
+
+ */
---
+++ service/import.go
@@ -0,0 +1,15 @@
+package service
+
+// Import service helps exports user data to another server
+type Import struct {
+}
+
+// NewImport returns a fully populated Import service
+func NewImport() Import {
+	return Import{}
+}
+
+/*
+
+
+ */
