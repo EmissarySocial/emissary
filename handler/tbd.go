b99269c93d60167cc560b177603506d4ac5cb381
Ben Pate <ben@pate.org>
2025-12-07 08:33:06 -0700

    TBD handler: send 404 Not Found errors
    Switching from "Not Implemented" to "Not Found" may improve compatability with other HTTP servers

--- handler/tbd.go
+++ handler/tbd.go
@@ -9,5 +9,5 @@ import (
 // TBD represents a placeholder handler that will be removed before release.
 // It returns a 501 "Not Implemented" status code.
 func TBD(ctx echo.Context) error {
-	return ctx.String(http.StatusNotImplemented, "Not Implemented")
+	return ctx.String(http.StatusNotFound, "Not Found")
 }
