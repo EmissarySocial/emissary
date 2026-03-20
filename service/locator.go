a4ac2eaa44fa70f4bbab7f1b38e7367ff111b004
Ben Pate <ben@pate.org>
2026-03-20 09:58:55 -0600

    Locator cleanup
    - be more forgiving of malformed WebFinger requests

--- service/locator.go
+++ service/locator.go
@@ -196,6 +196,7 @@ func locateObjectFromURL(host string, value string) (string, string) {
 	// Identify Username-type values
 	if value, found := strings.CutSuffix(value, "@"+hostname); found {
 
+		// Remove leading "@" if present
 		value = strings.TrimPrefix(value, "@")
 
 		// Special case for "Application" account
@@ -234,7 +235,7 @@ func locateObjectFromURL(host string, value string) (string, string) {
 			return model.ActorTypeApplication, ""
 		}
 
-		// Identify Gloabl Search actor
+		// Identify Global Search actor
 		if value == "@search" {
 			return model.ActorTypeSearchDomain, ""
 		}
@@ -254,5 +255,7 @@ func locateObjectFromURL(host string, value string) (string, string) {
 		return model.ActorTypeStream, value
 	}
 
-	return "", ""
+	// Last chance, assume we have a "naked" username. Try to
+	// find the user by looking up "value@hostname".
+	return locateObjectFromURL(host, value+"@"+hostname)
 }
