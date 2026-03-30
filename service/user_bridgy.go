22b5a0c32e9c75154a8cd1f2e19284cb3f086a22
Ben Pate <ben@pate.org>
2026-03-29 20:21:45 -0600

    Use correct Bridgy actor

--- service/user_bridgy.go
+++ service/user_bridgy.go
@@ -43,11 +43,11 @@ func (service *User) connectBluesky_follow(session data.Session, userID primitiv
 
 	const location = "service.User.connectBluesky_follow"
 
-	if _, err := service.followingService.Follow(session, userID, "@ap.brid.gy"); err != nil {
+	if _, err := service.followingService.Follow(session, userID, "@bsky.brid.gy@bsky.brid.gy"); err != nil {
 		return derp.Wrap(err, location, "Unable to follow Bridgy Fed Actor", userID, connection)
 	}
 
-	if err := service.ruleService.BlockActor(session, userID, "@ap.brid.gy", ""); err != nil {
+	if err := service.ruleService.BlockActor(session, userID, "@bsky.brid.gy@bsky.brid.gy", ""); err != nil {
 		return derp.Wrap(err, location, "Unable to unblock Bridgy Fed Actor", userID, connection)
 	}
 
@@ -59,11 +59,11 @@ func (service *User) connectBluesky_unfollow(session data.Session, userID primit
 
 	const location = "service.User.connectBluesky_unfollow"
 
-	if err := service.followingService.Unfollow(session, userID, "@ap.brid.gy"); err != nil {
+	if err := service.followingService.Unfollow(session, userID, "@bsky.brid.gy@bsky.brid.gy"); err != nil {
 		return derp.Wrap(err, location, "Unable to unfollow Bridgy Fed Actor", userID, connection)
 	}
 
-	if err := service.ruleService.UnblockActor(session, userID, "@ap.brid.gy"); err != nil {
+	if err := service.ruleService.UnblockActor(session, userID, "@bsky.brid.gy@bsky.brid.gy"); err != nil {
 		return derp.Wrap(err, location, "Unable to unblock Bridgy Fed Actor", userID, connection)
 	}
 
