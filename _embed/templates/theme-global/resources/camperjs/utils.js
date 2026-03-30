3f6100b74e2a7bd2f18bc797aa2bc5755f79f99f
Ben Pate <ben@pate.org>
2026-03-29 19:46:18 -0600

    CamperJS
    - Pure Javascript implementation of Activity Intents

---
+++ _embed/templates/theme-global/resources/camperjs/utils.js
@@ -0,0 +1,2 @@
+function c(t){switch(t){case"localhost":case"127.0.0.1":return"http://"}return"https://"}function r(t){return(t.match(/\{([^}]+)\}/g)||[]).map(e=>e.slice(1,-1))}export{r as getPlaceholders,c as guessProtocol};
+//# sourceMappingURL=utils.js.map
