8a79dcf6ed7d50669cb9e790209304327f9df52f
Ben Pate <ben@pate.org>
2026-04-10 11:47:24 -0600

    Remove calcRelationships

--- tools/ascache/client-relationships.go
+++ tools/ascache/client-relationships.go
@@ -4,61 +4,62 @@ import (
 	"github.com/benpate/data"
 	"github.com/benpate/derp"
 	"github.com/benpate/exp"
-	"github.com/benpate/rosetta/mapof"
-	"github.com/benpate/turbine/queue"
 )
 
 // CalcParentRelationships counts the number of records linked to a URL via a single relationType
 func (client *Client) CalcParentRelationships(session data.Session, relationType string, relationHref string) error {
 
-	const location = "ascache.client.CalcParentRelationships"
+	/*
+		const location = "ascache.client.CalcParentRelationships"
 
-	// RULE: If there is no relationship, then exit now
-	if relationType == "" {
-		return nil
-	}
-
-	// RULE: If there is no relationship, then exit now
-	if relationHref == "" {
-		return nil
-	}
-
-	// Try to load the "parent" item from the cache.
-	parentValue := NewValue()
-	if err := client.loadByURL(session, relationHref, &parentValue); err != nil {
-
-		// If the "parent" doesn't exist in the cache, then try to load it... later.
-		if derp.IsNotFound(err) {
-			client.queue.NewTask(
-				"CrawlActivityStreams",
-				mapof.Any{
-					"host":      client.hostname,
-					"actorType": client.actorType,
-					"actorID":   client.actorID,
-					"url":       relationHref,
-				},
-				queue.WithSignature(relationHref),
-			)
+		// RULE: If there is no relationship, then exit now
+		if relationType == "" {
 			return nil
 		}
 
-		// All other errors break the request.
-		return derp.Wrap(err, location, "Unable to load cached ActivityStream document", relationHref)
-	}
-
-	// Count the current number of related relationships
-	count, err := client.CountRelatedValues(session, relationType, relationHref)
-
-	if err != nil {
-		return derp.Wrap(err, location, "Error counting related values", relationHref, relationType)
-	}
-
-	// If the count has changed, then update the parent with the new count
-	if changed := parentValue.Metadata.SetRelationCount(relationType, count); changed {
-		if err := client.save(session.Context(), parentValue.DocumentID(), &parentValue); err != nil {
-			return derp.Wrap(err, location, "Unable to save updated ActivityStream document", relationHref)
+		// RULE: If there is no relationship, then exit now
+		if relationHref == "" {
+			return nil
 		}
-	}
+
+		// Try to load the "parent" item from the cache.
+		parentValue := NewValue()
+		if err := client.loadByURL(session, relationHref, &parentValue); err != nil {
+
+			// If the "parent" doesn't exist in the cache, then try to load it... later.
+			if derp.IsNotFound(err) {
+				client.queue.NewTask(
+					"CrawlActivityStreams",
+					mapof.Any{
+						"host":      client.hostname,
+						"actorType": client.actorType,
+						"actorID":   client.actorID,
+						"url":       relationHref,
+					},
+					queue.WithSignature(relationHref),
+				)
+				return nil
+			}
+
+			// All other errors break the request.
+			return derp.Wrap(err, location, "Unable to load cached ActivityStream document", relationHref)
+		}
+
+		// Count the current number of related relationships
+		count, err := client.CountRelatedValues(session, relationType, relationHref)
+
+		if err != nil {
+			return derp.Wrap(err, location, "Error counting related values", relationHref, relationType)
+		}
+
+		// If the count has changed, then update the parent with the new count
+		if changed := parentValue.Metadata.SetRelationCount(relationType, count); changed {
+			if err := client.save(session.Context(), parentValue.DocumentID(), &parentValue); err != nil {
+				return derp.Wrap(err, location, "Unable to save updated ActivityStream document", relationHref)
+			}
+		}
+
+	*/
 
 	// success.
 	return nil
