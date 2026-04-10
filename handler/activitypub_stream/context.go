1086312bdef2357d682f55130ba1d1b20a8fc0a7
Ben Pate <ben@pate.org>
2026-04-10 11:49:56 -0600

    Move context into router definition

--- handler/activitypub_stream/context.go
+++ handler/activitypub_stream/context.go
@@ -7,13 +7,6 @@ import (
 	"github.com/benpate/hannibal/outbox"
 )
 
-// Context includes all of the necessary objects to handle an ActivityPub request
-type Context struct {
-	factory *service.Factory
-	session data.Session
-	stream  *model.Stream
-	actor   *model.StreamActor
-}
 
 func (context Context) ActivityPubActor() (outbox.Actor, error) {
 
--- handler/activitypub_stream/router.go
+++ handler/activitypub_stream/router.go
@@ -1,6 +1,25 @@
 package activitypub_stream
 
-import "github.com/benpate/hannibal/router"
+import (
+	"github.com/EmissarySocial/emissary/model"
+	"github.com/EmissarySocial/emissary/service"
+	"github.com/benpate/data"
+	"github.com/benpate/hannibal/outbox"
+	"github.com/benpate/hannibal/router"
+)
 
 // streamRouter defines the package-level router for stream/ActivityPub requests
 var streamRouter = router.New[Context]()
+
+// Context includes all of the necessary objects to handle an ActivityPub request
+type Context struct {
+	factory *service.Factory
+	session data.Session
+	stream  *model.Stream
+	actor   *model.StreamActor
+}
+
+func (context Context) ActivityPubActor() (outbox.Actor, error) {
+
+	return context.factory.Stream().ActivityPubActor(context.session, context.stream.StreamID)
+}
