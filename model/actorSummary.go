98ed4d2f24b5ce185b87da61fb5bd71bdc70c0f7
Ben Pate <ben@pate.org>
2026-04-11 12:08:16 -0600

    Use standard "preferredUsername"

--- model/actorSummary.go
+++ model/actorSummary.go
@@ -6,19 +6,19 @@ import (
 
 // ActorSummary is a record returned by the ActivityStream directory
 type ActorSummary struct {
-	ID             string `json:"id"              bson:"id"`
-	Type           string `json:"type"            bson:"type"`
-	Name           string `json:"name"            bson:"name"`
-	Username       string `json:"username"        bson:"username"`
-	Icon           string `json:"icon"            bson:"icon"`
-	MLSKeyPackages string `json:"mls:keyPackages" bson:"mls:keyPackages"`
+	ID                string `json:"id"                bson:"id"`
+	Type              string `json:"type"              bson:"type"`
+	Name              string `json:"name"              bson:"name"`
+	PreferredUsername string `json:"preferredUsername" bson:"preferredUsername"`
+	Icon              string `json:"icon"              bson:"icon"`
+	MLSKeyPackages    string `json:"mls:keyPackages"   bson:"mls:keyPackages"`
 }
 
 // UsernameOrID returns the best identifier we can find for an Actor:
 // either the Actor' username, if it exists, or the Actor's ID
 func (actor ActorSummary) UsernameOrID() string {
-	if actor.Username != "" {
-		return "@" + actor.Username + "@" + dt.NameOnly(actor.ID)
+	if actor.PreferredUsername != "" {
+		return "@" + actor.PreferredUsername + "@" + dt.NameOnly(actor.ID)
 	}
 
 	return actor.ID
