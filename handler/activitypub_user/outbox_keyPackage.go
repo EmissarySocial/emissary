598a8611ea4ce15c16d6d0df2a6bc8b17b51bbd7
Ben Pate <ben@pate.org>
2026-04-20 09:14:33 -0600

    User Inbox: Add route to update KeyPackages

--- handler/activitypub_user/outbox_keyPackage.go
+++ handler/activitypub_user/outbox_keyPackage.go
@@ -12,6 +12,7 @@ import (
 
 func init() {
 	outboxRouter.Add(vocab.ActivityTypeCreate, vocab.ObjectTypeKeyPackage, outbox_CreateKeyPackage)
+	outboxRouter.Add(vocab.ActivityTypeUpdate, vocab.ObjectTypeKeyPackage, outbox_UpdateKeyPackage)
 	outboxRouter.Add(vocab.ActivityTypeAdd, vocab.ObjectTypeKeyPackage, outbox_AddKeyPackage)
 	outboxRouter.Add(vocab.ActivityTypeRemove, vocab.ObjectTypeKeyPackage, outbox_RemoveKeyPackage)
 	outboxRouter.Add(vocab.ActivityTypeDelete, vocab.ObjectTypeKeyPackage, outbox_DeleteKeyPackage)
@@ -50,6 +51,52 @@ func outbox_CreateKeyPackage(context Context, activity streams.Document) error {
 	return context.context.NoContent(http.StatusCreated)
 }
 
+// Update an existing KeyPackage record from the ActivityPub API
+func outbox_UpdateKeyPackage(context Context, activity streams.Document) error {
+
+	const location = "handler.activitypub_user.outbox_UpdateKeyPackage"
+
+	object := activity.Object()
+
+	// RULE: The object must be attributed to the actor
+	if object.AttributedTo().ID() != activity.Actor().ID() {
+		return derp.Forbidden(location, "KeyPackage must be attributed to the actor", activity.Value())
+	}
+
+	// Locate the existing KeyPackage
+	keyPackageService := context.factory.MLSKeyPackage()
+	keyPackage := model.NewKeyPackage()
+
+	if err := keyPackageService.LoadByURL(context.session, object.ID(), &keyPackage); err != nil {
+		return derp.Wrap(err, location, "Unable to load KeyPackage", "url", object.ID())
+	}
+
+	// RULE: Guarantee that the KeyPackage belongs to the user making this request
+	if keyPackage.UserID != context.user.UserID {
+		return derp.Forbidden(location, "KeyPackage must be owned by this actor")
+	}
+
+	// RULE: Guarantee that the KeyPackage was created by the same Generator (device)
+	if keyPackage.GeneratorID != object.Generator().ID() {
+		return derp.Forbidden(location, "KeyPackage must be created by the same Generator")
+	}
+
+	// But you can update these values...
+	keyPackage.MediaType = object.MediaType()
+	keyPackage.Encoding = object.Encoding()
+	keyPackage.Content = object.Content()
+	keyPackage.GeneratorName = object.Generator().Name()
+
+	// Save the KeyPackage to the database
+	if err := keyPackageService.Save(context.session, &keyPackage, "Created via ActivityPub API"); err != nil {
+		return derp.Wrap(err, location, "Unable to save KeyPackage")
+	}
+
+	// Write the response to the context
+	context.context.Response().Header().Set("Location", keyPackageService.ActivityPubURL(keyPackage.UserID, keyPackage.KeyPackageID))
+	return context.context.NoContent(http.StatusCreated)
+}
+
 // Locate and delete the KeyPackage referenced in the ActivityPub request
 func outbox_DeleteKeyPackage(context Context, activity streams.Document) error {
 
@@ -63,11 +110,18 @@ func outbox_DeleteKeyPackage(context Context, activity streams.Document) error {
 		return derp.Forbidden(location, "KeyPackage must be owned by this actor")
 	}
 
-	// Load the KeyPackage
+	// Try to load the KeyPackage
 	keyPackageService := context.factory.MLSKeyPackage()
 	keyPackage := model.NewKeyPackage()
 
 	if err := keyPackageService.LoadByURL(context.session, object.ID(), &keyPackage); err != nil {
+
+		// If the KeyPackage doesn't exist, then "I have already won..."
+		if derp.IsNotFound(err) {
+			return nil
+		}
+
+		// Otherwise, you suck. I won't delete this KeyPackage for you.
 		return derp.Wrap(err, location, "Unable to load KeyPackage", "url", object.ID())
 	}
 
