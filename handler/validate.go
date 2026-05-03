f101154a6a67c38a7b006f5a59807b4572b80451
Ben Pate <ben@pate.org>
2026-05-03 16:58:39 -0600

    Add Stream token validation

--- handler/validate.go
+++ handler/validate.go
@@ -3,6 +3,7 @@ package handler
 import (
 	"net/http"
 
+	"github.com/EmissarySocial/emissary/model"
 	"github.com/EmissarySocial/emissary/service"
 	"github.com/benpate/data"
 	"github.com/benpate/derp"
@@ -73,15 +74,50 @@ func GetValidateUsername(ctx *steranko.Context, factory *service.Factory, sessio
 // GetValidateStreamToken validates a Stream.Token for uniqueness/availability
 func GetValidateStreamToken(ctx *steranko.Context, factory *service.Factory, session data.Session) error {
 
-	/*
-		// This service can only validate the "token" field
-		if field := ctx.QueryParam("field"); field != "token" {
-			return ctx.JSON(http.StatusBadRequest, mapof.Any{
-				"valid":   false,
-				"message": "Invalid field",
+	token := ctx.QueryParam("value")
+
+	if len(token) < 3 {
+		return ctx.JSON(http.StatusOK, mapof.Any{
+			"valid":   false,
+			"message": "Token must be at least 3 characters",
+		})
+	}
+
+	// This service can only validate the "token" field
+	if field := ctx.QueryParam("field"); field != "token" {
+		return ctx.JSON(http.StatusBadRequest, mapof.Any{
+			"valid":   false,
+			"message": "Invalid field",
+		})
+	}
+
+	// Collect variables
+	streamService := factory.Stream()
+	stream := model.NewStream()
+
+	if err := streamService.LoadByToken(session, ctx.QueryParam("value"), &stream); err != nil {
+
+		if derp.IsNotFound(err) {
+			return ctx.JSON(http.StatusOK, mapof.Any{
+				"valid":   true,
+				"message": "",
 			})
 		}
-	*/
 
-	return derp.NotImplemented("handler.GetValidateStreamToken")
+		return derp.Wrap(err, "handler.GetValidateStreamToken", "Error loading stream by token")
+	}
+
+	// If there is no match, then the token is valid
+	if stream.ID() == ctx.QueryParam("streamId") {
+		return ctx.JSON(http.StatusOK, mapof.Any{
+			"valid":   true,
+			"message": "",
+		})
+	}
+
+	// Otherwise, the token is taken
+	return ctx.JSON(http.StatusOK, mapof.Any{
+		"valid":   false,
+		"message": "This token is already in use by another stream",
+	})
 }
