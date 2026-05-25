a4cde009d7b02304c9c7f392eb8c0f39a285a093
Ben Pate <ben@pate.org>
2026-05-25 12:34:30 -0600

    Add proxyUrl into actor definition

--- model/user.go
+++ model/user.go
@@ -248,6 +248,7 @@ func (user User) GetJSONLD() mapof.Any {
 			vocab.EndpointOAuthToken:         serverURL + "/oauth/token",
 			vocab.EndpointStartMigration:     serverURL + "/@" + user.UserID.Hex() + "/export/start",
 			vocab.EndpointFinishMigration:    serverURL + "/@me/settings/export",
+			vocab.EndpointProxyURL:           serverURL + "/@" + user.UserID.Hex() + "/pub/proxy",
 		},
 
 		vocab.PropertyMigration: mapof.String{
