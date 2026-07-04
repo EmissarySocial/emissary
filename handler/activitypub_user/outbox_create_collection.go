03773fb70b37dd1b054e63789c950b7c0861bbbb
Ben Pate <ben@pate.org>
2026-07-04 00:46:55 -0600

    Comments

--- handler/activitypub_user/outbox_create_collection.go
+++ handler/activitypub_user/outbox_create_collection.go
@@ -32,6 +32,12 @@ func outbox_CreateOrderedCollection(context Context, activity streams.Document) 
 		return derp.Wrap(err, location, "Unable to save collection", collection)
 	}
 
+	// RULE: Unlike other Create handlers (Article, Note, KeyPackage), a Collection is NOT
+	// pushed through the outbox via putActivityIntoOutbox.  A Collection is a pull resource:
+	// it is served from GetCollection at /pub/collections/{id} and read access is gated by
+	// its To/Cc participant lists (see Collection.HasParticipant / canViewCollection).
+	// Participants fetch it on demand; it is not federated as a standalone activity.
+
 	// Write the response to the client
 	context.context.Response().Header().Set("Location", collectionService.ActivityPubURL(collection.UserID, collection.CollectionID))
 	return context.context.NoContent(http.StatusCreated)
