d4fcd3e56384cd498ef44b353ef30d3423e746df
Ben Pate <ben@pate.org>
2026-07-08 20:21:30 -0600

    CollectionItem: add model fields

--- model/collectiontem.go
+++ model/collectiontem.go
@@ -10,6 +10,8 @@ type CollectionItem struct {
 	CollectionItemID primitive.ObjectID `bson:"collectionItemId,omitempty"` // Unique ID of a document in this database
 	CollectionID     primitive.ObjectID `bson:"collectionId,omitempty"`     // Unique ID of the parent Collection document in this database
 	UserID           primitive.ObjectID `bson:"userId,omitempty"`           // Unique ID of the User who owns the Collection
+	ParentID         primitive.ObjectID `bson:"parentId,omitempty"`         // Unique ID of the parent document in this database
+	Type             string             `bson:"type,omitempty"`             // Type of collection (Context, Replies, etc.)
 	URI              string             `bson:"uri,omitempty"`              // Public URI of the CollectionItem
 	InReplyTo        string             `bson:"inReplyTo,omitempty"`        // Public URI of the item this CollectionItem is replying to
 
