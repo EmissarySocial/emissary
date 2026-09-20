0c6bd1b8e1ac033863faa54ac0867d454c8f8584
Ben Pate <ben@pate.org>
2026-09-20 11:27:06 -0600

    Streams: Sharing Status

---
+++ model/stream_constants.go
@@ -0,0 +1,9 @@
+package model
+
+// Sharing status tokens returned by Stream.SharingStatus
+const (
+	SharingStatusPublic        = "PUBLIC"
+	SharingStatusAuthenticated = "AUTHENTICATED"
+	SharingStatusCircles       = "CIRCLES"
+	SharingStatusOwners        = "OWNERS"
+)
